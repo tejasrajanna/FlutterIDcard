d0aa45adba439caf55faa4632e1bf48fd8bea4bf
Tejas R <tejasrajanna@gmail.com>
2021-07-13 08:49:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Card(),
+  ));
+}
+
+class Card extends StatelessWidget {
+  const Card ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/avatar.jpeg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color:Colors.grey[700],
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+              ),
+              SizedBox(height: 10,),
+              Text(
+              "Tejas R",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              SizedBox(height: 30),
+               Text(
+              "CURRENT SEMESTER",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+              ),
+              Text(
+              "4",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey
+                    ),
+                    SizedBox(width: 10,),
+                    Text(
+                      'tejasrajanna@gmail.com',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 18,
+                      )
+                    ),
+                ],
+              ),
+          ],) ,
+      ),
+    );
+  }
+}
+
